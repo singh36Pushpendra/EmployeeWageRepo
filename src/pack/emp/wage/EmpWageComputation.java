b10dc638554d63fb359777babfb59174b03477df
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-17 18:19:14 +0530

    Added: Employee daily wage

--- src/pack/emp/wage/EmpWageComputation.java
+++ src/pack/emp/wage/EmpWageComputation.java
@@ -5,17 +5,23 @@ import java.util.Random;
 public class EmpWageComputation {
 
 	public static void main(String[] args) {
+		float dailyWage;
+		int wagePerHour = 20, fullDayHour = 0;
 		
 		System.out.println("Welcome to Employee Wage Computation Program.");
 
 		Random random = new Random();
 		boolean attendance = random.nextBoolean();
 		
-		if (attendance)
+		if (attendance) {
 			System.out.println("Employee is present.");
+			fullDayHour = 8;
+		}
 		else
 			System.out.println("Employee is absent.");
-		
+
+		dailyWage = wagePerHour * fullDayHour;
+		System.out.println("Daily wage of employee: " + dailyWage);
 	}
 
 }
