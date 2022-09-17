3cde0cc69d93328fb64e15a1c4ba115d025a6f6e
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-17 17:29:50 +0530

    modified: welcome message

--- src/pack/emp/wage/EmpWageComputation.java
+++ src/pack/emp/wage/EmpWageComputation.java
@@ -3,7 +3,8 @@ package pack.emp.wage;
 public class EmpWageComputation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Employee Wage Computation Program.");
 
 	}
 
