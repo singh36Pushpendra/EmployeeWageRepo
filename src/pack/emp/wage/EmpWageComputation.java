b8573aa7cf8486735b8ca252aee0d91da6fbe729
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-29 20:57:14 +0530

    Refactored: UC7 class methods and variables

--- src/pack/emp/wage/EmpWageComputation.java
+++ src/pack/emp/wage/EmpWageComputation.java
@@ -4,42 +4,52 @@ import java.util.Random;
 
 public class EmpWageComputation {
 
-	public static void main(String[] args) {
-		float dailyWage, monthlyWage = 0;
-		int wagePerHour = 20, fullDayHour, workingDaysPerMonth = 20, day = 0;
+	private static final int PART_TIME = 1, FULL_TIME = 2, WAGE_PER_HOUR = 20;
 
-		System.out.println("Welcome to Employee Wage Computation Program.");
-
-		Random random = new Random();
-		int empPerDay;
-		int workingHoursPerMonth = 0;
+	private static float dailyWage, monthlyWage;
+	private static int empPerDay, fullDayHour, workingHoursPerMonth, day; 
+	private static Random random = new Random();
+	
+	private static void findEmpWage() {
+		workingHoursPerMonth = 0;
+		monthlyWage = 0;
+		day = 0;
+		System.out.println("\nWelcome to Employee Wage Computation Program.");
+		
 		while (workingHoursPerMonth != 100 && day != 20) {
 			day++;
 			empPerDay = random.nextInt(3);
 
 			switch (empPerDay) {
 
-				case 0:
-					fullDayHour = 0;
-					System.out.println("Employee is absent.");
-					break;
-				case 1:
-					fullDayHour = 4;
-					System.out.println("Part time employee is present.");
-					break;
-				default:
-					fullDayHour = 8;
-					System.out.println("Full time employee is present.");
-					
+			case PART_TIME:
+				fullDayHour = 4;
+				System.out.println("Part time employee is present.");
+				break;
+
+			case FULL_TIME:
+				fullDayHour = 8;
+				System.out.println("Full time employee is present.");
+				break;
+
+			default:
+				fullDayHour = 0;
+				System.out.println("Employee is absent.");
 			}
-			
+
 			workingHoursPerMonth += fullDayHour;
-			dailyWage = wagePerHour * fullDayHour;
+			dailyWage = WAGE_PER_HOUR * fullDayHour;
 			monthlyWage += dailyWage;
+			
 			System.out.println("Day " + day + " wage of employee: " + dailyWage + "\n");
 		}
-		System.out.println("\nMonth wage of employee: " + monthlyWage);
+		
+		System.out.println("\nMonthly wage of employee: " + monthlyWage);
 		System.out.println("\nProgram runs total: " + day + " times.");
+	}
+
+	public static void main(String[] args) {
+		findEmpWage();
 	}
 
 }
