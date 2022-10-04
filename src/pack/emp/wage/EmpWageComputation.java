b37e8bbdf55f890857b6d6feab633fdcee6cc1f5
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-04 21:50:10 +0530

    Added: UC9 EmpWageBuilder & refactored EmpWageComputation

--- src/pack/emp/wage/EmpWageComputation.java
+++ src/pack/emp/wage/EmpWageComputation.java
@@ -1,84 +1,50 @@
 package pack.emp.wage;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class EmpWageComputation {
 
-	private static final int PART_TIME = 1, FULL_TIME = 2;
-
-	private static float wagePerHour, dailyWage, monthlyWage;
-	private static int empPerDay, fullDayHour, workingHoursPerMonth, day, countCompany; 
-	private static Random random = new Random();
 	private static Scanner scanner = new Scanner(System.in);
-	private static int perCompanyHours, perCompanyDays;
-	
-	private static void findEmpWage(float wagePerHour, int totalWorkingDays, int totalWorkingHours) {
-		workingHoursPerMonth = 0;
-		monthlyWage = 0;
-		day = 0;
-		System.out.println("\nWelcome to Employee Wage Computation Program.");
-		
-		while (workingHoursPerMonth != totalWorkingHours && day != totalWorkingDays) {
-			day++;
-			empPerDay = random.nextInt(3);
-
-			switch (empPerDay) {
-
-			case PART_TIME:
-				fullDayHour = 4;
-				System.out.println("Part time employee is present.");
-				break;
-
-			case FULL_TIME:
-				fullDayHour = 8;
-				System.out.println("Full time employee is present.");
-				break;
-
-			default:
-				fullDayHour = 0;
-				System.out.println("Employee is absent.");
-			}
-
-			workingHoursPerMonth += fullDayHour;
-			dailyWage = wagePerHour * fullDayHour;
-			monthlyWage += dailyWage;
-			
-			System.out.println("Day " + day + " wage of employee: " + dailyWage + "\n");
-		}
-
-		System.out.println("\nWorking hours per month: " + workingHoursPerMonth);
-		System.out.println("\nMonthly wage of employee: " + monthlyWage);
-		System.out.println("\nProgram runs total: " + day + " times.");
-	}
+	private static String companyName;
+	private static float wagePerHour;
+	private static int perCompanyDays, perCompanyHours, countCompany;
+	private static EmpWageBuilder[] companies;
 
 	private static void takeCompanyDetails() {
 		countCompany++;
+		System.out.println("\n--------------------------------------------------");
 		System.out.println("Enter company " + countCompany + " details: ");
-		System.out.println("Enter per hour wage for company:");
+		System.out.println("Enter company name: ");
+		companyName = scanner.nextLine();
+		System.out.println("Enter company per hour wage:");
 		wagePerHour = scanner.nextFloat();
-		System.out.println("Enter total working days per month for company: ");
+		System.out.println("Enter company total working days per month: ");
 		perCompanyDays = scanner.nextInt();
-		System.out.println("Enter total working hours per month for company: ");
+		System.out.println("Enter company total working hours per month: ");
 		perCompanyHours = scanner.nextInt();
+		
+		scanner.nextLine();
 	}
 	
 	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage Computation Program.");
 		System.out.println("Enter number for how many companies you wan't to find employee wage: ");
 		int num = scanner.nextInt();
-		float[] companiesWages = new float[num];
-		int[] companiesDays = new int[num];
-		int[] companiesHours = new int[num];
+		scanner.nextLine();
+		
+		companies = new EmpWageBuilder[num];
 		
 		for (int i = 0; i < num; i++) {
 			takeCompanyDetails();
-			companiesWages[i] = wagePerHour;
-			companiesDays[i] = perCompanyDays;
-			companiesHours[i] = perCompanyHours;
+			companies[i] = new EmpWageBuilder(companyName, wagePerHour, perCompanyDays, perCompanyHours);
 		}
 		
+		System.out.println("\n\n--------------------------------------------------");
+		
 		for (int i = 0; i < num; i++) {
-			findEmpWage(companiesWages[i], companiesDays[i], companiesHours[i]);
+			companies[i].findEmpWage();
+			System.out.println("\n\n--------------------------------------------------");
 		}
 	}
 
